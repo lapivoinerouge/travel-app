4a163c1f1191cdd05311d3e293d9706f809a24cb
lapivoinerouge <wioladys@gmail.com>
2020-04-25 09:58:45 +0200

    fix: use mini-css-extract-plugin to load styles

--- webpack.prod.js
+++ webpack.prod.js
@@ -25,11 +25,11 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader' ]
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             },
             {
                 test: /\.(png|jpe?g|gif)$/i,
